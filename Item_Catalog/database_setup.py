e0aa7bf0762a451a1fab6b73206f4a30b44b558e
Satya Madhu <45555784+satyamadhu09@users.noreply.github.com>
2019-02-02 14:49:21 +0530

    Checked under pep8 tool

--- Item_Catalog/database_setup.py
+++ Item_Catalog/database_setup.py
@@ -2,7 +2,7 @@ import os
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship,backref
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -12,27 +12,25 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable = False)
-    email = Column(String(250), nullable = False)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
     picture = Column(String(250))
 
 
 class GameGenre(Base):
     __tablename__ = 'games'
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(250), nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref="games")
-    
-
 
     @property
     def serialize(self):
         return {
-        'id':self.id,
-        'name': self.name,
-        'user_id': self.user_id,
+                'id': self.id,
+                'name': self.name,
+                'user_id': self.user_id,
         }
 
 
@@ -44,19 +42,19 @@ class ListGame(Base):
     description = Column(String(250))
     price = Column(String(8))
     game_id = Column(Integer, ForeignKey('games.id'))
-    games = relationship(GameGenre, backref=backref('list_game', cascade='all, delete'))
+    games = relationship(GameGenre, backref=backref('list_game',
+                                                    cascade='all, delete'))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref="list_game")
 
     @property
     def serialize(self):
         return {
-        'id':self.id,
-        'name': self.name,
-        'description': self.description,
-        'price': self.price,
+                'id': self.id,
+                'name': self.name,
+                'description': self.description,
+                'price': self.price,
         }
-
 
 
 engine = create_engine('sqlite:///gamelist.db')
